960e151411a116fc46033885945389cf3c6a1310
lagbullet <40629577+lagbullet@users.noreply.github.com>
2018-09-16 21:55:08 +0300

    1

---
+++ BookApp/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace BookApp
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ReleaseDate { get; set; }
+
+        public Book() { }
+        public Book(string name, string author, string releasedate)
+        {
+            Name = name;
+            Author = author;
+            ReleaseDate = releasedate;
+        }
+        public string ToXML()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                new XmlSerializer(typeof(Book)).Serialize(sw, this);
+                return sw.ToString();
+            }
+        }
+
+        public static Book FromXML(string xmlText)
+        {
+            using (StringReader stringReader = new StringReader(xmlText))
+                return new XmlSerializer(typeof(Book)).Deserialize(stringReader) as Book; 
+        }
+
+        public string ToJSON()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        public static Book FromJSON(string jsondata)
+        {
+            return JsonConvert.DeserializeObject<Book>(jsondata);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Book Name - {0}, Author - {1}, Release Date - {2}",Name,Author,ReleaseDate);
+        }
+
+    }
+}
---
+++ BookApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace BookApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"E:\Test\TestJson.txt");
+            Book book = Book.FromJSON(text);
+            book.Show();
+            Console.WriteLine(book.ToJSON());
+            Console.ReadLine();
+            text = File.ReadAllText(@"E:\Test\TestXml.txt");
+            Book book1 = Book.FromXML(text);
+            book1.Show();
+            Console.WriteLine(book1.ToXML());
+            Console.ReadLine();
+        }
+    }
+}
